f6ce40a4f3ee5498f1b8d0e0472b9f15c81b4b76
jun <junkaiong11@gmail.com>
2018-12-15 19:13:12 -0800

    Define hardware specification

---
+++ lc3.c
@@ -0,0 +1,47 @@
+#define MAX_MEMORY UINT64_MAX
+#define R_COUNT 10
+
+/* Memory */
+uint16_t memory[MAX_MEMORY];
+
+/* Registers */
+enum
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7
+};
+
+/* Instruction Set */
+enum
+{
+    OP_BR = 0, /* branch */
+    OP_ADD,    /* add  */
+    OP_LD,     /* load */
+    OP_ST,     /* store */
+    OP_JSR,    /* jump register */
+    OP_AND,    /* bitwise and */
+    OP_LDR,    /* load register */
+    OP_STR,    /* store register */
+    OP_RTI,    /* unused */
+    OP_NOT,    /* bitwise not */
+    OP_LDI,    /* load indirect */
+    OP_STI,    /* store indirect */
+    OP_JMP,    /* jump */
+    OP_RES,    /* reserved (unused) */
+    OP_LEA,    /* load effective address */
+    OP_TRAP    /* execute trap */
+};
+
+/* Conditional Flags */
+enum
+{
+    FL_POS = 1 << 0, /* P */
+    FL_ZRO = 1 << 1, /* Z */
+    FL_NEG = 1 << 2, /* N */
+};
